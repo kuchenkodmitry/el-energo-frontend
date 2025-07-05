26c493b92d0440069218a63c4d6b4bc62c840db0
Kuchenko Dmitriy <161463592+kuchenkodmitry@users.noreply.github.com>
2025-07-05 22:00:21 +0300

    Revert "chore: remove mongoose and unused server code (#12)"
    This reverts commit ad42849a799cbf1e2fee4a95d3108570d447144a.

---
+++ server/controllers/PostContoller.js
@@ -0,0 +1,127 @@
+import PostModel from "../models/Post.js"
+
+export const getAll = async (req, res) => {
+  try {
+    const posts = await PostModel.find().populate('user').exec(); //Вернуть все почты .populate('user').exec() - вместе с информацией о пользователе
+    res.json(posts)
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "не удалось получить посты"
+    })
+  }
+}
+
+export const create = async (req, res) => {
+  try {
+    const doc = new PostModel({
+      title: req.body.title,
+      text: req.body.text,
+      imageUrl: req.body.imageUrl,
+      tags: req.body.tags,
+      user: req.userId,
+      gallaryUrl: req.body.gallaryUrl,
+      order: req.body.order
+    });
+
+    const post = await doc.save();
+
+    res.json(post);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Не удалось создать статью',
+    });
+  }
+};
+
+export const getOne = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    const getOnePost = await PostModel.findOneAndUpdate(
+      {
+        _id: postId,
+      },
+      {
+        $inc: { viewsCount: 1 },
+      },
+      {
+        returnDocument: 'after',
+      },
+
+    ).then((doc, err) => {
+        if(err){
+         return res.status(404).json({
+            message: 'Не удалось найти статью',
+          })
+        }
+        if(!doc){
+          return res.status(404).json({
+            message: "Статья была удалена или не существут"
+          })
+        }
+        return res.status(200).json(doc)
+      },)
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Не удалось получить статью',
+    });
+  }
+};
+
+
+export const remove = async (req, res) => {
+  try{
+    const postId = req.params.id;
+
+    await PostModel.findOneAndDelete(
+      {
+        _id: postId,
+      },).then((doc, err) => {
+        if(err){
+          return res.status(404).json({
+            message: 'Не удалось найти статью',
+          })
+        }
+        res.status(200).json({
+          message: "Статья удалена", 
+          doc
+        })
+      })
+  } catch {
+    console.log(err);
+    res.status(500).json({
+      message: 'Не удалось получить статью',
+    });
+  }
+}
+
+export const update = async (req, res) => {
+  try{
+    const postId = req.params.id;
+
+    await PostModel.updateOne({
+      _id: postId,
+    },{
+      title: req.body.title,
+      text: req.body.text,
+      imageUrl: req.body.imageUrl,
+      tags: req.body.tags,
+      user: req.userId,
+      order: req.body.order
+    }).then((doc, err) => {
+      res.json({
+        message: "Данные успешно изменены",
+      })
+    })
+
+    
+  } catch(err){
+    console.log(err);
+    res.status(500).json({
+      message: 'Не удалось изменить данные',
+    });
+  }
+}
